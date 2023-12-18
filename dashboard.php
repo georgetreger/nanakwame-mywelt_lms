78435010487a2dd235b28a730aa06e30b3c5fd8d
Eben <ebendevs@gmail.com>
2023-12-18 08:46:06 +0000

    updated dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,210 @@
+<?php include './header.html';
+include './nav.php';
+?>
+
+    <div class="row">
+        <div class="row">
+            <div class="col-12 mt-4">
+                <div class="mb-5 ps-3">
+                    <button class="btn btn-primary" type="button">All</button>
+                    <button class="btn btn-primary" type="button">Next.js</button>
+                    <button class="btn btn-primary" type="button">React.js</button>
+                    <button class="btn btn-primary" type="button">MySQL</button>
+                    <button class="btn btn-primary" type="button">MongoDB</button>
+                    <button class="btn btn-primary" type="button">Prisma</button>
+                    <button class="btn btn-primary" type="button">Tailwind</button>
+                    <button class="btn btn-primary" type="button">Node.js</button>
+                    <button class="btn btn-primary" type="button">Supabase</button>
+                </div>
+                <div class="row">
+                    <div class="col-xl-3 col-md-6 mb-xl-0 mb-5">
+                        <div class="card card-background-mask-dark ">
+                            <a class="" href="https://google.com">
+                                <div class="card-header p-0 mt-n4 mx-3">
+                                    <a class="d-block shadow-xl border-radius-xl">
+                                        <img src="./assets/img/home-decor-1.jpg" alt="img-blur-shadow"
+                                             class="img-fluid shadow border-radius-xl">
+                                    </a>
+                                </div>
+                                <div class="card-body p-3">
+                                    <a href="javascript:;">
+                                        <h5>
+                                            Twitch Clone
+                                        </h5>
+                                    </a>
+                                    <p class="mb-0 text-sm">Vera</p>
+                                    <p class="mb-4 text-sm">
+                                        In this free 14 hour tutorial you are going to learn how to build a Twitch Clone
+                                        using Next 14.
+                                    </p>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <h6 class="text-decoration-underline bg-outline-danger mb-0">FREE
+                                        </h6>
+                                        <div class="avatar-group mt-2">
+                                            <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                               data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                               title="Elena Morison">
+                                                <img alt="Image placeholder" src="./assets/img/team-1.jpg">
+                                            </a>
+                                            <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                               data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                                                <img alt="Image placeholder" src="./assets/img/team-2.jpg">
+                                            </a>
+                                            <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                               data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                                                <img alt="Image placeholder" src="./assets/img/team-3.jpg">
+                                            </a>
+                                            <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                               data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                                                <img alt="Image placeholder" src="./assets/img/team-4.jpg">
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-3 col-md-6 mb-xl-0 mb-5">
+                        <div class="card card-background-mask-dark ">
+                            <div class="card-header p-0 mt-n4 mx-3">
+                                <a class="d-block shadow-xl border-radius-xl">
+                                    <img src="./assets/img/home-decor-2.jpg" alt="img-blur-shadow"
+                                         class="img-fluid shadow border-radius-lg">
+                                </a>
+                            </div>
+                            <div class="card-body p-3">
+                                <a href="javascript:;">
+                                    <h5>
+                                        Trello Clone
+                                    </h5>
+                                </a>
+                                <p class="mb-0 text-sm">Ebenezer</p>
+                                <p class="mb-4 text-sm">
+                                    In this 12 hour tutorial you will learn how to create an end-to-end fullstack and
+                                    trello clone, all with workspaces, boards, lists, cards, audit logs / activity as
+                                    well as member roles
+                                </p>
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <button type="button" class="btn btn-outline-primary btn-sm mb-0">View Project
+                                    </button>
+                                    <div class="avatar-group mt-2">
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                                            <img alt="Image placeholder" src="./assets/img/team-3.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                                            <img alt="Image placeholder" src="./assets/img/team-4.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                                            <img alt="Image placeholder" src="./assets/img/team-1.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                                            <img alt="Image placeholder" src="./assets/img/team-2.jpg">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 mb-xl-0 mb-5">
+                        <div class="card card-background-mask-dark ">
+                            <div class="card-header p-0 mt-n4 mx-3">
+                                <a class="d-block shadow-xl border-radius-xl">
+                                    <img src="./assets/img/home-decor-3.jpg" alt="img-blur-shadow"
+                                         class="img-fluid shadow border-radius-xl">
+                                </a>
+                            </div>
+                            <div class="card-body p-3">
+
+                                <a href="javascript:;">
+                                    <h5>
+                                        FullStack Notion Clone
+                                    </h5>
+                                </a>
+                                <p class="mb-0 text-sm">Nana Kwame</p>
+                                <p class="mb-4 text-sm">
+                                    In this 8 hour tutorial you will learn how to create an end-to-end fullstack notion
+                                    clone, all with proper notion-style editor, cover images, nested documents,
+                                    publishing documents to public, real-time database and more.
+                                </p>
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <button type="button" class="btn btn-outline-primary btn-sm mb-0">View Project
+                                    </button>
+                                    <div class="avatar-group mt-2">
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                                            <img alt="Image placeholder" src="./assets/img/team-4.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                                            <img alt="Image placeholder" src="./assets/img/team-3.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                                            <img alt="Image placeholder" src="./assets/img/team-2.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                                            <img alt="Image placeholder" src="./assets/img/team-1.jpg">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 mb-xl-0 mb-5">
+                        <div class="card card-background-mask-dark ">
+                            <div class="card-header p-0 mt-n4 mx-3">
+                                <a class="d-block shadow-xl border-radius-xl">
+                                    <img src="https://images.unsplash.com/photo-1606744824163-985d376605aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+                                         alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
+                                </a>
+                            </div>
+                            <div class="card-body p-3">
+                                <a href="javascript:;">
+                                    <h5>
+                                        FullStack Discord Clone
+                                    </h5>
+                                </a>
+                                <p class="mb-0 text-sm">Joel</p>
+                                <p class="mb-4 text-sm">
+                                    In this 11 hour course you will learn how to create an end-to-end fullstack and
+                                    real-time discord clone, all with servers, channels, video calls, audio calls,
+                                    editing and deleting messages as well as member roles.
+                                </p>
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <button type="button" class="btn btn-outline-primary btn-sm mb-0">View Project
+                                    </button>
+                                    <div class="avatar-group mt-2">
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
+                                            <img alt="Image placeholder" src="./assets/img/team-4.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
+                                            <img alt="Image placeholder" src="./assets/img/team-3.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
+                                            <img alt="Image placeholder" src="./assets/img/team-2.jpg">
+                                        </a>
+                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle"
+                                           data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
+                                            <img alt="Image placeholder" src="./assets/img/team-1.jpg">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php include './footer.html' ?>
